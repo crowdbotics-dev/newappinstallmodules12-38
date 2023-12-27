1f270734192e8b835733dbe8cf19fc1b5a7c6f3a
Crowdbotics <engineering@crowdbotics.com>
2023-12-27 15:57:24 +0000

    Update store.

--- store/newappinstallmodulesAPI/api.js
+++ store/newappinstallmodulesAPI/api.js
@@ -1,72 +1,61 @@
-import axios from "axios";
+import axios from "axios"
 const newappinstallmodulesAPI = axios.create({
   baseURL: "https://newappinstallmodules12-38.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return newappinstallmodulesAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return newappinstallmodulesAPI.post(`/api/v1/login/`, payload);
+  return newappinstallmodulesAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return newappinstallmodulesAPI.post(`/api/v1/signup/`, payload);
+  return newappinstallmodulesAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/login/`, payload);
+  return newappinstallmodulesAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/logout/`);
+  return newappinstallmodulesAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/password/change/`, payload);
+  return newappinstallmodulesAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/password/reset/`, payload);
+  return newappinstallmodulesAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return newappinstallmodulesAPI.post(
+    `/rest-auth/password/reset/confirm/`,
+    payload
+  )
 }
-
 function rest_auth_registration_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/registration/`, payload);
+  return newappinstallmodulesAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return newappinstallmodulesAPI.post(
+    `/rest-auth/registration/resend-email/`,
+    payload
+  )
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return newappinstallmodulesAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return newappinstallmodulesAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return newappinstallmodulesAPI.get(`/rest-auth/user/`);
+  return newappinstallmodulesAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return newappinstallmodulesAPI.put(`/rest-auth/user/`, payload);
+  return newappinstallmodulesAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return newappinstallmodulesAPI.patch(`/rest-auth/user/`, payload);
+  return newappinstallmodulesAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +71,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
